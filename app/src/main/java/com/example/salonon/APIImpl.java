147168ebdbe2e1117ddbaea5e1e2eff5b83ccfcf
tmam101 <thomaspgoss98@gmail.com>
2019-10-07 13:12:08 -0400

    Create seperate Network class to handle implementation.  It returns strings that are JSON parseable.

--- app/src/main/java/com/example/salonon/APIImpl.java
+++ app/src/main/java/com/example/salonon/APIImpl.java
@@ -16,8 +16,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class APIImpl implements API {
-    private String herokuURL = "https://salon-on-backend.herokuapp.com/";
-    private String herokuTestURL = "https://salon-on.herokuapp.com/";
+    Network network = new Network();
+
     public String createNewProfile(Profile profileToAddToDatabase) {
         return null;
     }
@@ -47,17 +47,12 @@ public class APIImpl implements API {
 
     public Profile getClientProfile(int clientID) {
         try {
-            NetworkPost task = new NetworkPost();
-            // Place the relevant parameters; in this case, "clientID" and their values (which must be strings)
-            task.parameters.put("clientID", String.valueOf(clientID));
-            // Execute the post request and receive the result from the server
-            String result = task.execute(herokuTestURL + "test").get();
-            // Convert the result from the server to a json object. This represents a profile.
-            JSONObject jsonObject = new JSONObject(result);
-            // Access firstName by using its key
-            String firstName = (String) jsonObject.get("firstName");
-            // Attach firstName to Profile.
-            // TODO return profile.
+            Map<String, String> parameters = new HashMap<>();
+            parameters.put("clientID", String.valueOf(clientID));
+            String response = network.post(network.herokuTestURL + "test", parameters);
+            JSONObject jsonProfile = new JSONObject(response);
+            String firstName = (String) jsonProfile.get("firstName");
+            // TODO Get all of the values and create a profile from them.
             return null;
         } catch (Exception e) {
             Exception thisException = e;
@@ -65,108 +60,5 @@ public class APIImpl implements API {
         }
     }
     // Given a client id, return a Profile Object of the client's data.
-
-    // Network code must be run asynchronously.  This AsyncTask technique might have bugs
-    // For example, rotating the phone might crash it.  We can adjust this later
-    class NetworkGet extends AsyncTask<String, Void, String> {
-
-        private Exception exception;
-
-        // Override method provided by asynctask.  This code is run upon NetworkGet.execute().
-        protected String doInBackground(String... urls) {
-            try {
-                // Create a connection to the "test" endpoint on our server.
-                URL url = new URL(urls[0]);
-                HttpURLConnection con = (HttpURLConnection) url.openConnection();
-                con.setRequestMethod("GET");
-                con.setRequestProperty("Content-Type", "application/json");
-
-                // Read the content returned from that endpoint.
-                BufferedReader in = new BufferedReader(
-                        new InputStreamReader(con.getInputStream()));
-                String inputLine;
-                StringBuilder content = new StringBuilder();
-                while ((inputLine = in.readLine()) != null) {
-                    content.append(inputLine);
-                }
-                in.close();
-                con.disconnect();
-                // Return the string.
-                return content.toString();
-            } catch (Exception e) {
-                this.exception = e;
-                return null;
-            }
-        }
-
-//        // TODO this might be useful, but so far I'm not using it.
-//        protected void onPostExecute(String string) {
-////            print(string);
-//        }
-
-    }
-
-    class NetworkPost extends AsyncTask<String, Void, String> {
-
-        private Exception exception;
-        public Map<String, String> parameters = new HashMap<>();
-
-        // Override method provided by asynctask.  This code is run upon NetworkGet.execute().
-        protected String doInBackground(String... urls) {
-            try {
-                // Create a connection to the "test" endpoint on our server.
-                URL url = new URL(urls[0]);
-                HttpURLConnection con = (HttpURLConnection) url.openConnection();
-                con.setRequestMethod("POST");
-
-                con.setDoOutput(true);
-                DataOutputStream out = new DataOutputStream(con.getOutputStream());
-                out.writeBytes(ParameterStringBuilder.getParamsString(parameters));
-                out.flush();
-                out.close();
-
-                // Read the content returned from that endpoint.
-                BufferedReader in = new BufferedReader(
-                        new InputStreamReader(con.getInputStream()));
-                String inputLine;
-                StringBuilder content = new StringBuilder();
-                while ((inputLine = in.readLine()) != null) {
-                    content.append(inputLine);
-                }
-                in.close();
-                con.disconnect();
-                // Return the string.
-                return content.toString();
-            } catch (Exception e) {
-                this.exception = e;
-                return null;
-            }
-        }
-    }
-
-    // Takes the Network's parameters and converts them to something that the server is able to parse via JSON.
-    public static class ParameterStringBuilder {
-        public static String getParamsString(Map<String, String> params)
-                throws UnsupportedEncodingException {
-            StringBuilder result = new StringBuilder();
-
-            result.append("{");
-            for (Map.Entry<String, String> entry : params.entrySet()) {
-                result.append("\"");
-                result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
-                result.append("\"");
-                result.append(":");
-                result.append("\"");
-                result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
-                result.append("\"");
-                result.append(",");
-            }
-
-            String resultString = result.toString();
-            return resultString.length() > 0
-                    ? resultString.substring(0, resultString.length() - 1) + "}"
-                    : resultString + "}";
-        }
-    }
 }
 
---
+++ app/src/main/java/com/example/salonon/Network.java
@@ -0,0 +1,139 @@
+package com.example.salonon;
+
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Network {
+    public String herokuURL = "https://salon-on-backend.herokuapp.com/";
+    public String herokuTestURL = "https://salon-on.herokuapp.com/";
+
+    public String get(String url) {
+        try {
+            NetworkGet task = new NetworkGet();
+            return task.execute(url).get();
+        } catch (Exception e){
+            return null;
+        }
+    }
+
+    // When creating a post request, first create a HashMap of <String, String> parameters and pass it in.
+    public String post(String url, Map<String, String> parameters) {
+        try {
+            NetworkPost task = new NetworkPost();
+            task.parameters = parameters;
+            return task.execute(url).get();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    // Network code must be run asynchronously.  This AsyncTask technique might have bugs
+    // For example, rotating the phone might crash it.  We can adjust this later
+    class NetworkGet extends AsyncTask<String, Void, String> {
+
+        private Exception exception;
+
+        // Override method provided by asynctask.  This code is run upon NetworkGet.execute().
+        protected String doInBackground(String... urls) {
+            try {
+                // Create a connection to the "test" endpoint on our server.
+                URL url = new URL(urls[0]);
+                HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                con.setRequestMethod("GET");
+                con.setRequestProperty("Content-Type", "application/json");
+
+                // Read the content returned from that endpoint.
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(con.getInputStream()));
+                String inputLine;
+                StringBuilder content = new StringBuilder();
+                while ((inputLine = in.readLine()) != null) {
+                    content.append(inputLine);
+                }
+                in.close();
+                con.disconnect();
+                // Return the string.
+                return content.toString();
+            } catch (Exception e) {
+                this.exception = e;
+                return null;
+            }
+        }
+
+//        // TODO this might be useful, but so far I'm not using it.
+//        protected void onPostExecute(String string) {
+////            print(string);
+//        }
+
+    }
+
+    private class NetworkPost extends AsyncTask<String, Void, String> {
+
+        private Exception exception;
+        public Map<String, String> parameters = new HashMap<>();
+
+        // Override method provided by asynctask.  This code is run upon NetworkGet.execute().
+        protected String doInBackground(String... urls) {
+            try {
+                // Create a connection to the "test" endpoint on our server.
+                URL url = new URL(urls[0]);
+                HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                con.setRequestMethod("POST");
+
+                con.setDoOutput(true);
+                DataOutputStream out = new DataOutputStream(con.getOutputStream());
+                out.writeBytes(getParametersString(parameters));
+                out.flush();
+                out.close();
+
+                // Read the content returned from that endpoint.
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(con.getInputStream()));
+                String inputLine;
+                StringBuilder content = new StringBuilder();
+                while ((inputLine = in.readLine()) != null) {
+                    content.append(inputLine);
+                }
+                in.close();
+                con.disconnect();
+                // Return the string.
+                return content.toString();
+            } catch (Exception e) {
+                this.exception = e;
+                return null;
+            }
+        }
+    }
+
+    // Takes the Network's parameters and converts them to something that the server is able to parse via JSON.
+    private String getParametersString(Map<String, String> parameters)
+            throws UnsupportedEncodingException {
+        StringBuilder result = new StringBuilder();
+
+        result.append("{");
+        for (Map.Entry<String, String> entry : parameters.entrySet()) {
+            result.append("\"");
+            result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+            result.append("\"");
+            result.append(":");
+            result.append("\"");
+            result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+            result.append("\"");
+            result.append(",");
+        }
+
+        String resultString = result.toString();
+        return resultString.length() > 0
+                ? resultString.substring(0, resultString.length() - 1) + "}"
+                : resultString + "}";
+    }
+}
