619417de9e1cafd4b36df96d4a30b15fbb19d53c
David <dmoore42n81@gmail.com>
2019-10-06 23:17:53 -0400

    Added more information to Booking class

--- app/src/main/java/com/example/salonon/Booking.java
+++ app/src/main/java/com/example/salonon/Booking.java
@@ -17,7 +17,7 @@ public class Booking {
         Profile clientProfile; // Pointer to the Client's Profile
         String salonName; // The name of the Salon
         String salonAddress; // The address of the Salon
-        String salonProfilePicture; // Salon profile Picture.
+        Image salonProfilePicture; // Salon profile Picture.
         Profile salonProfile; // Pointer to the Salon's Profile
         String dateOfAppointment; // The date of the appointment
         String timeOfAppointment; // The time of the appointment
@@ -32,18 +32,75 @@ public class Booking {
         String requestedService; // Requested service of the booking.
         String timeToCompleteService; // Time to complete the service of the booking
 
-    public Booking(@Nullable Profile clientProfile, @Nullable Profile stylistProfile, @Nullable Profile salonProfile) {
-        if(clientProfile != null) {
-            // set client information
+    public Booking(Profile clientProfile, Profile stylistProfile, @Nullable Profile salonProfile, String dateOfAppointment, String timeOfAppointment, @Nullable String clientCommentsOnBooking, @Nullable String stylistCommentsOnBooking, @Nullable String salonCommentsOnBooking, String requestedService) {
+        // set stylist information
+        this.stylistName = stylistProfile.firstName + stylistProfile.lastName;
+        this.stylistProfilePicture = stylistProfile.profilePicture;
+        this.stylistProfile = stylistProfile;
+        this.timeToCompleteService = null; // TODO Implement a stylist Profile that extends Profile which has a timeToCompleteService for each service offered.
 
-        }
-        if(stylistProfile != null) {
-            // set stylist information
-        }
+        // set client information
+        this.clientName = clientProfile.firstName + clientProfile.lastName;
+        this.clientProfilePicture = clientProfile.profilePicture;
+        this.clientProfile = clientProfile;
+
+
         if(salonProfile != null) {
             // set salon information
+            this.salonName = null; //TODO implement a Salon Profile that extends Profile with a salonName field.
+            this.salonAddress = null; // TODO implement a Salon Profile that extends Profile with a salonAddress field.
+            this.salonProfilePicture = salonProfile.profilePicture;
+            this.salonProfile = salonProfile;
         }
 
+        // General, appointment specific information:
+        this.bookingStatus = BookingStatus.PENDING; // Booking is Pending when first initialized.
+
+        if(dateOfAppointment != null) {
+            this.dateOfAppointment = dateOfAppointment;
+        } else {
+            this.dateOfAppointment = "January 1st, 1970";
+        }
+
+        if(timeOfAppointment != null) {
+            this.timeOfAppointment = timeOfAppointment;
+        } else {
+            this.timeOfAppointment = "12:00";
+        }
+
+        if (clientCommentsOnBooking != null) {
+            this.clientCommentsOnBooking = clientCommentsOnBooking;
+        } else {
+            this.clientCommentsOnBooking = "No Comment";
+        }
+
+        if (stylistCommentsOnBooking != null) {
+            this.stylistCommentsOnBooking = stylistCommentsOnBooking;
+        } else {
+            this.stylistCommentsOnBooking = "No Comment";
+        }
+
+        if(salonCommentsOnBooking != null) {
+            this.salonCommentsOnBooking = salonCommentsOnBooking;
+        } else {
+            this.salonCommentsOnBooking = "No Comment";
+        }
+
+
+    }
+
+    public void addSalonProfileToBooking(Profile salonProfile, @Nullable String salonCommentsOnBooking) {
+        // set salon information
+        this.salonName = null; //TODO implement a Salon Profile that extends Profile with a salonName field.
+        this.salonAddress = null; // TODO implement a Salon Profile that extends Profile with a salonAddress field.
+        this.salonProfilePicture = salonProfile.profilePicture;
+        this.salonProfile = salonProfile;
+
+        if(salonCommentsOnBooking != null) {
+            this.salonCommentsOnBooking = salonCommentsOnBooking;
+        } else {
+            this.salonCommentsOnBooking = "No Comment";
+        }
     }
 
 
