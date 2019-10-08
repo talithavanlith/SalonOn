c0c53b2ba9300446cda369634728562d153b9f7f
David <dmoore42n81@gmail.com>
2019-10-07 22:38:23 -0400

    Added Beginnings of Sign in activity

---
+++ app/src/main/java/com/example/salonon/SignInActivity.java
@@ -0,0 +1,14 @@
+package com.example.salonon;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
