c39ed4c3bd15fd3d820ef2ed36b011ff3d7fe96e
talithavanlith <talithavanlith@outlook.com>
2019-12-05 12:31:51 -0500

    Finalised all design aspects of the app on what we have done so far.

--- app/src/main/java/com/example/salonon/ProfileActivity.java
+++ app/src/main/java/com/example/salonon/ProfileActivity.java
@@ -3,6 +3,7 @@ package com.example.salonon;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
