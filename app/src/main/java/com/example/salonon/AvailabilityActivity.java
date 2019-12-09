a269097fa77dfca601bd554995a9e3888272f289
talithavanlith <talithavanlith@outlook.com>
2019-12-08 21:11:52 -0500

    Correct details appear on the availability screen

--- app/src/main/java/com/example/salonon/AvailabilityActivity.java
+++ app/src/main/java/com/example/salonon/AvailabilityActivity.java
@@ -1,13 +1,59 @@
 package com.example.salonon;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AvailabilityActivity extends AppCompatActivity {
+
+    private API api;
+    private Profile stylist;
+    private String offerID;
+    private String style;
+    private Double time;
+    private Double price;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_availablity);
+
+        // get extras from passed intent:
+        Intent currentIntent = getIntent();
+        Bundle bundle = currentIntent.getExtras();
+        String email = bundle.getString("stylist");
+        offerID = bundle.getString("offerID");
+        style = bundle.getString("style");
+        time = bundle.getDouble("duration");
+        price = bundle.getDouble("price");
+
+        api = new API();
+        stylist = api.getClientProfile(email);
+
+        loadInfoTEST();
+    }
+
+    private void loadInfoTEST(){
+        //ACCESS THE ELEMENTS IN THE INFLATED VIEW (THIS IS WHERE WE EDIT THEM)
+        ImageView image = this.findViewById(R.id.imgProfile);
+        TextView name = this.findViewById(R.id.txtStylistName);
+
+        TextView styleName = this.findViewById(R.id.txtStyle);
+
+        TextView timeView = this.findViewById(R.id.txtTime);
+        TextView priceView = this.findViewById(R.id.txtPrice);
+
+        // set the details
+        //todo: add image
+        name.setText(stylist.first + " " + stylist.last);
+
+        styleName.setText(style);
+
+        timeView.setText(""+ time +" hrs");
+        priceView.setText("$" + price);
+
     }
 }
--- app/src/main/java/com/example/salonon/StyleInfoActivity.java
+++ app/src/main/java/com/example/salonon/StyleInfoActivity.java
@@ -15,6 +15,9 @@ public class StyleInfoActivity extends AppCompatActivity {
     private API api;
     private Profile stylist;
     private String offerID;
+    private String style;
+    private Double time;
+    private Double price;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +29,9 @@ public class StyleInfoActivity extends AppCompatActivity {
         Bundle bundle = currentIntent.getExtras();
         String email = bundle.getString("stylist");
         offerID = bundle.getString("offerID");
-        String style = bundle.getString("style");
-        Double time = bundle.getDouble("duration");
-        Double price = bundle.getDouble("price");
+        style = bundle.getString("style");
+        time = bundle.getDouble("duration");
+        price = bundle.getDouble("price");
 
         api = new API();
 
@@ -64,12 +67,18 @@ public class StyleInfoActivity extends AppCompatActivity {
     }
 
     public void bookNowButtonOnClick(View v) {
+
         // Create activity_profile Intent;
-        Intent availabilityIntent = new Intent(StyleInfoActivity.this, AvailabilityActivity.class);
+        Intent styleInfoIntent = new Intent(StyleInfoActivity.this, AvailabilityActivity.class);
         // todo: below get the name of the style that is clicked (and maybe the stylist's email/id)
-//        styleInfoIntent.putExtra("stylist", v.getStylistEmail());
-//        styleInfoIntent.putExtra("style", v.getStyleID());
-        startActivity(availabilityIntent);
+        styleInfoIntent.putExtra("stylist", stylist.email);
+//        // Create activity_profile Intent;
+        styleInfoIntent.putExtra("style", style);
+        styleInfoIntent.putExtra("duration", time);
+        styleInfoIntent.putExtra("price", price);
+        styleInfoIntent.putExtra("offerID", offerID);
+
+        startActivity(styleInfoIntent);
 
     }
 }
